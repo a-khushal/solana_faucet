c2d124a095b7280274aee09d89d2ff20d8521b71
khushal <khushal2004a@gmail.com>
2024-04-23 00:01:01 +0530

    added hono

---
+++ backend/src/index.ts
@@ -0,0 +1,44 @@
+import { Hono } from 'hono'
+import * as web3 from '@solana/web3.js';
+
+const app = new Hono()
+
+app.get("/", (c)=>{
+  return c.json({
+    message: "server running"
+  })
+})
+
+app.post('/', async (c) => {
+  try{
+    let connection = new web3.Connection(web3.clusterApiUrl("devnet"), "confirmed");
+    const payer = web3.Keypair.generate();
+    const body = await c.req.json();
+    const toAccount = body.toPublicKey;
+    const airdropSignature = await connection.requestAirdrop(
+      payer.publicKey,
+      web3.LAMPORTS_PER_SOL,
+    );
+    // @ts-ignore
+    await connection.confirmTransaction({ signature: airdropSignature })
+    let transaction = new web3.Transaction();
+    transaction.add(
+      web3.SystemProgram.transfer({
+        fromPubkey: payer.publicKey,
+        toPubkey: toAccount,
+        lamports: 1000,
+      }),
+    );
+    const confirm = await web3.sendAndConfirmTransaction(connection, transaction, [payer]);
+    return c.json({
+      confirm
+    })
+  } catch(err) {
+      console.log(err);
+      return c.json({
+        error: err
+      })
+    }
+})
+
+export default app
